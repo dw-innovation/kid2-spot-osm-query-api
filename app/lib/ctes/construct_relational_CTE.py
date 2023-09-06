c7d6fe5974feb502cf84f02b3556a0520f345baa
tlmn <mail@tlmn.io>
2023-09-06 09:31:15 +0200

    add nearby nodes

--- app/lib/ctes/construct_relational_CTE.py
+++ app/lib/ctes/construct_relational_CTE.py
@@ -67,18 +67,48 @@ def construct_relation(edge, nodes):
                         SELECT *, 'src' AS origin FROM {src_CTE_name}
                         UNION ALL
                         SELECT *, 'tgt' AS origin FROM {tgt_CTE_name}
+                    ),
+                    Distances AS (
+                        SELECT c1.osm_ids AS src_id, array_agg(DISTINCT c2.osm_ids) AS tgt_ids,
+                            c1.tags AS tags, c1.geom AS geom
+                        FROM setNodes AS c1
+                        JOIN setNodes AS c2 ON ST_DWithin(c1.transformed_geom, c2.transformed_geom, {dist_in_meters})
+                        AND c1.set_id <> c2.set_id
+                        GROUP BY c1.osm_ids, c1.tags, c1.geom
+                    ),
+                    ReverseDistances AS (
+                        SELECT c2.osm_ids AS tgt_id, array_agg(DISTINCT c1.osm_ids) AS src_ids,
+                            c2.tags AS tags, c2.geom AS geom
+                        FROM setNodes AS c1
+                        JOIN setNodes AS c2 ON ST_DWithin(c1.transformed_geom, c2.transformed_geom, {dist_in_meters})
+                        AND c1.set_id <> c2.set_id
+                        GROUP BY c2.osm_ids, c2.tags, c2.geom
+                    ),
+                    Combined AS (
+                        SELECT 
+                            COALESCE(d.src_id, rd.tgt_id) AS node_id,
+                            COALESCE(d.tags, rd.tags) AS tags,
+                            COALESCE(d.geom, rd.geom) AS geom,
+                            d.tgt_ids AS src_nearby,
+                            rd.src_ids AS tgt_nearby
+                        FROM Distances d
+                        FULL OUTER JOIN ReverseDistances rd ON d.src_id = rd.tgt_id
                     )
-                    SELECT c1.*
-                    FROM setNodes AS c1
-                    JOIN setNodes AS c2 ON ST_DWithin(c1.transformed_geom, c2.transformed_geom, {dist_in_meters})
-                                        AND c1.set_id <> c2.set_id
-                )"""
+                    SELECT 
+                        node_id, tags, geom,
+                        ARRAY(
+                            SELECT DISTINCT unnest(ARRAY_CAT(COALESCE(src_nearby, ARRAY[]::bigint[]), COALESCE(tgt_nearby, ARRAY[]::bigint[])))
+                        ) AS nearby_node_ids
+                    FROM Combined
+            )"""
         ).format(
             relation_CTE_name=sql.Identifier(relation_CTE_name),
             src_CTE_name=sql.Identifier(src_CTE_name),
             tgt_CTE_name=sql.Identifier(tgt_CTE_name),
             utm=sql.Literal(g.utm),
             dist_in_meters=sql.Literal(dist_in_meters),
+            src_id=sql.Literal(source_id),
+            target_id=sql.Literal(target_id),
         )
 
     # Check if the edge type is "cnt"
@@ -97,28 +127,42 @@ def construct_relation(edge, nodes):
         tgt_CTE_name = f"{tgt_type}_{tgt_set['id']}_{tgt_set['n']}".replace(" ", "_")
 
         relational_query = sql.SQL(
-            """
-                {relation_CTE_name} AS (
+            """{relation_CTE_name} AS (
                     WITH setNodes AS (
                         SELECT *, 'src' AS origin FROM {src_CTE_name}
                         UNION ALL
                         SELECT *, 'tgt' AS origin FROM {tgt_CTE_name}
                     ),
                     Contained AS (
-                        SELECT c1.*
+                        SELECT c1.osm_ids AS contained_id, c1.tags AS contained_tags, c1.geom AS contained_geom, array_agg(c2.osm_ids) AS container_ids
                         FROM setNodes AS c1
                         JOIN setNodes AS c2 ON ST_Contains(c2.transformed_geom, c1.transformed_geom) 
                                             AND c1.set_id <> c2.set_id
+                        GROUP BY c1.osm_ids, c1.tags, c1.geom
                     ),
                     Containers AS (
-                        SELECT c2.*
-                        FROM Contained AS c1
+                        SELECT c2.osm_ids AS container_id, c2.tags AS container_tags, c2.geom AS container_geom, array_agg(c1.osm_ids) AS contained_ids
+                        FROM setNodes AS c1
                         JOIN setNodes AS c2 ON ST_Contains(c2.transformed_geom, c1.transformed_geom) 
                                             AND c1.set_id <> c2.set_id
+                        GROUP BY c2.osm_ids, c2.tags, c2.geom
+                    ),
+                    Combined AS (
+                        SELECT 
+                            COALESCE(cd.contained_id, cn.container_id) AS node_id,
+                            COALESCE(cd.contained_tags, cn.container_tags) AS tags,
+                            COALESCE(cd.contained_geom, cn.container_geom) AS geom,
+                            cd.container_ids AS src_nearby,
+                            cn.contained_ids AS tgt_nearby
+                        FROM Contained cd
+                        FULL OUTER JOIN Containers cn ON cd.contained_id = cn.container_id
                     )
-                    SELECT * FROM Contained
-                    UNION ALL
-                    SELECT * FROM Containers
+                    SELECT 
+                        node_id, tags, geom,
+                        ARRAY(
+                            SELECT DISTINCT unnest(ARRAY_CAT(COALESCE(src_nearby, ARRAY[]::bigint[]), COALESCE(tgt_nearby, ARRAY[]::bigint[])))
+                        ) AS nearby_node_ids
+                    FROM Combined
                 )
                 """
         ).format(
