dea05f1089a066f2be2ed57c19ccc16a5b24d802
tlmn <mail@tlmn.io>
2023-09-08 11:17:26 +0200

    add spots

--- app/app.py
+++ app/app.py
@@ -4,13 +4,11 @@ from flask import Flask, request, jsonify, g
 from flask_cors import CORS
 from jsonschema import validate, exceptions
 import psycopg2
-from lib.utils import set_area
-from lib.utils import results_to_geojson
+from lib.utils import get_spots, set_area, results_to_geojson
 from lib.database import initialize_connection_pool, get_db, close_db
 import lib.constructor as constructor
 from collections import Counter
 from lib.timer import Timer
-from psycopg2 import sql
 
 environment = os.getenv("ENVIRONMENT") or "production"
 
@@ -73,21 +71,22 @@ def run_osm_query():
 
     try:
         set_area(data)
-        timer.add_checkpoint("area_set")
+        timer.add_checkpoint("area_setting")
 
         query = constructor.construct_query_from_graph(data)
 
-        timer.add_checkpoint("query_constructed")
+        timer.add_checkpoint("query_construction")
 
         cursor = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cursor.execute(query)
-        timer.add_checkpoint("query_executed")
+        timer.add_checkpoint("query_execution")
 
         # Fetch all results as a list of dictionaries
         results = [dict(record) for record in cursor]
 
+        spots = get_spots(results)
         geojson = results_to_geojson(results)
-        timer.add_checkpoint("results_transformed_to_geojson")
+        timer.add_checkpoint("results_transformation_to_geojson")
 
         distinct_set_names = list({result["set_name"] for result in results})
         set_name_counts = dict(Counter(result["set_name"] for result in results))
@@ -102,6 +101,7 @@ def run_osm_query():
             ),
             **({"area": area_value} if area_value is not None else {}),
             "sets": {"distinct_sets": distinct_set_names, "stats": set_name_counts},
+            "spots": spots,
             "timing": timer.get_all_checkpoints(),
         }
 
